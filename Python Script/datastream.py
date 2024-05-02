b4eabeb44f0f2faea0ef59787364fab391ae6c8b
SakkyOP <saksham298@gmail.com>
2024-05-02 21:57:29 +0530

    recorded database

--- Python Script/datastream.py
+++ Python Script/datastream.py
@@ -42,7 +42,7 @@ serialInst.port = portVar
 serialInst.open()
 
 record_data = False  # Flag to indicate whether to record data or not
-start_time = None  # Variable to store the start time of recording
+start_time = 0  # Variable to store the start time of recording
 label = None
 samples_recorded = 0
 
@@ -54,7 +54,7 @@ while True:
             samples_recorded = 0
         print("Recording data...")
         data_buffer = []  # Buffer to store data during recording
-        timestamp_buffer = []  # Buffer to store timestamps during recording
+        timestamp_buffer: list[float] = []  # Buffer to store timestamps during recording
         directory = create_directories(label)
         sample_number = get_next_sample_number(directory)
         start_time = time.time()  # Start time of recording
@@ -63,7 +63,7 @@ while True:
         record_data = False
         if len(data_buffer) > 0:
             # Save the recorded data as a binary file
-            timestamp_buffer = [int((t - start_time) * 1000) for t in timestamp_buffer]  # Convert to milliseconds
+            timestamp_buffer = [(t - start_time) * 1000 for t in timestamp_buffer]  # Convert to milliseconds
             data_buffer_with_timestamp = np.column_stack((timestamp_buffer, data_buffer))
             filename = os.path.join(directory, f'sample_{sample_number}.npy')
             np.save(filename, data_buffer_with_timestamp)
